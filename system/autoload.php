c8b24ff2d4cb46f09221c01f7eda6bd863c6ba11
buhala <fix288@gmail.com>
2013-05-03 16:14:32 +0300

    Update autoload.php
    Fixed a critical error in the class.

--- system/autoload.php
+++ system/autoload.php
@@ -1,7 +1,8 @@
 <?php
 //Input libraries to autoload right here!
-$libraries=array('database');
-foreach($libraries as $lib){
+//If you are using a library, type in global $libraries whereever you are using it
+$libraries_list=array('database');
+foreach($libraries_list as $lib){
     include 'libs/'.$lib.'.php';
     $libraries[$lib]=new $lib;
 }
